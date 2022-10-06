a42577000bdb17f91caf7f8d904e4f41b405e4e8
Jenna Reps <jreps@its.jnj.com>
2022-10-06 16:39:54 -0400

    lego approach
    - revised the code from created UI and server text to a more lego approach

--- R/ViewShiny.R
+++ R/ViewShiny.R
@@ -6,16 +6,20 @@
 #' @details
 #' User specifies the shiny app location and this function opens it
 #' 
-#' @param shinyAppLocation  The folder to create the shiny app in                                       
+#' @param config  The json with the app config                            
 #' @return
 #' The shiny app will open
 #'
 #' @export
-viewShiny <- function(shinyAppLocation){
-  # check settings
+viewShiny <- function(config){
   
-  # open up the app
-  shiny::runApp(shinyAppLocation) 
+  if(missing(config)){
+    ParallelLogger::logInfo('Using default config')
+    config <- ParallelLogger::loadSettingsFromJson(system.file('shiny', 'config.json', package = 'shinyModuleViewer'))
+  }
+  
+  app <- shiny::shinyApp(ui(config=config), server(config = config))
+  shiny::runApp(app)
 }
 
 
